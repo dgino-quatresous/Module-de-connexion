9dd15b1c232c4d7a8950a6d69f2a0c2b39795841
dgino <dgino.quatresous@laplateforme.io>
2025-10-03 17:45:17 +0200

    module_test2

--- Projet/connexion.php
+++ Projet/connexion.php
@@ -5,30 +5,11 @@ require_once 'src/db.php';
 try {
     require_once 'src/db.php';
     $connection = connect_pdo();
-
-    $check_sql = 'SELECT id FROM utilisateurs WHERE login = ?';
-    $check_stmt = $connection->prepare($check_sql);
-    $check_stmt->execute([$login]);
-
-    if ($check_stmt->rowCount() > 0) {
-        $error_message = "Ce nom d'utilisateur existe déjà.";
-    }else{
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO utiliseurs(login, nom, prenom, password) VALUES (?, ?, ?, ?)";
-        $inscription = $connection->prepare($sql);
-        $inscription->execute([$login, $nom, $prenom, $password]);
-
-
-        $success_message = "Inscription réussie! Redirection vers la page de connexion...";
-        header("location:connexion.php");
-    }}catch (PDOException $e) {
-        if ($e->getCode() == 23000) {
-            $error_message = "Ce nom d'utilisateur est déjà utilisé!";
-        }else{
-            $error_message = "Erreur lors de l'inscription: " . $e->getMessage();
+}catch (PDOException $e) {
+            if ($e->getCode() == 23000) {
+                $error_message = "Ce nom d'utilisateur est déjà utilisé!";
+            }
         }
-    }
-
 
 ?>
 
@@ -43,6 +24,7 @@ try {
     <form action="index.php" method="post">
         <input type="text" name="login" id="login" placeholder="Nom d'utilisateur" required>
         <input type="text" name="password" id="password" placeholder="Mot de passe" required>
+        <input type="submit">
     </form>
 </body>
 </html>
@@ -52,15 +34,15 @@ try {
 
 function connection_verification() {
     if ($_POST["login"] == $login) {
-        if ($_POST["password"] == $password) {
-            $connecte = true;
-            
-        }else{
+        function password_verify($password, $hashedPassword) {
+            if ($password == $hashedPassword) {
+                $connecte = true;
+            }else{
             echo "Mot de passe invalide.";
         }
+    }
     }else{
         echo "Nom d'utilisateur invalide.";
-    }
-}
+    }}
 
 ?>
